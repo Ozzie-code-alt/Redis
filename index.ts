72fda1a8e9d7502f5b8f6bd4f2ab66e9aad464c3
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-11-15 22:49:09 +0800

    add prettier and index. js port listenenr

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
 import express from "express"
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+app.use(express.json())
+
+
+app.listen(PORT, ()=>{
+    console.log(`Appluication running on port ${PORT}`)
+}).on("error", (error)=>{
+    throw new Error(error.message)
+})
